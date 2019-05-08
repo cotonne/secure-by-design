3d868a4a2a6c9f3d26a742f7c907bd8e2ae36889
Cotonne <cotonne@github.com>
2019-05-08 12:28:57 +0200

    Docker support

--- HacmeBooks/run.sh
+++ HacmeBooks/run.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-java -cp lib/hsqldb/hsqldb.jar org.hsqldb.server.Server --database.0 file:mydb --dbname.0 xdb &
+BASE=$(dirname "$0")
+cd $BASE
+
+java -cp ./hsqldb-2.4.1.jar org.hsqldb.server.Server --database.0 file:mydb --dbname.0 xdb &
 
 sleep 10
 
