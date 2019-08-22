2d377bd410f8916ae0fb06e7929f3b3265cf3456
Cotonne <cotonne@github.com>
2019-08-22 22:30:59 +0200

    Add validation in form

--- HackmeBooks2/hackmebooks2-front/src/app/signup/signup.component.ts
+++ HackmeBooks2/hackmebooks2-front/src/app/signup/signup.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {environment} from '../../environments/environment';
 import {HttpClient} from '@angular/common/http';
 import {Router} from '@angular/router';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 class SignupModel {
   username: string;
@@ -21,22 +22,37 @@ class SignupModel {
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
-
-  private model: SignupModel;
+  form = new FormGroup({
+    email: new FormControl('', [
+      Validators.required,
+      Validators.email,
+    ]),
+    username: new FormControl('', [Validators.required]),
+    password: new FormControl('', Validators.required),
+    password_confirmation: new FormControl('', Validators.required),
+    first_name: new FormControl('', [Validators.required]),
+    last_name: new FormControl('', [Validators.required]),
+    phone_number: new FormControl(),
+    ssn: new FormControl(),
+    password_hint: new FormControl()
+  }, {validators: passwordMatchValidator});
 
   constructor(private httpClient: HttpClient, private router: Router) {
   }
 
   ngOnInit() {
-    this.model = new SignupModel();
   }
 
   signup() {
-    console.log(this.model);
     this.httpClient
-      .post(`${environment.host}/signup`, this.model)
+      .post(`${environment.host}/signup`, this.form.value)
       .subscribe(() => this.router.navigate(['/login']),
         () => {
         });
   }
+}
+
+function passwordMatchValidator(g: FormGroup) {
+  return g.get('password').value === g.get('password_confirmation').value
+    ? null : {'mismatch': true};
 }
